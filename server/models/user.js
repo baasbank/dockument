f78f2a2553fedc62f202a7150d1a9e09e809f082
Baasbank <baasb4real@yahoo.com>
2017-07-16 20:35:07 +0100

    chore(generate-and-migrate-models): create the user model

---
+++ server/models/user.js
@@ -0,0 +1,56 @@
+// import bcrypt from 'bcrypt-nodejs';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          message: 'Name field cannot be empty.',
+        },
+        is: /^[a-z ]+$/i,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          message: 'Please input a valid email address.',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          message: 'Password field cannot be empty.',
+        },
+        len: [4, 20],
+      },
+    },
+    roleType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    classMethods: {
+      associate: (models) => {
+        User.hasMany(models.Document, {
+          foreignKey: 'UserId',
+          as: 'documents',
+        });
+
+        User.belongsTo(models.Role, {
+          foreignKey: 'roleType',
+          onDelete: 'CASCADE',
+        });
+      }
+    }
+  });
+  return User;
+};
+
