8923fe79dd9cdb20cc8d8ae9c77052f961f28c2d
Baasbank <baasb4real@yahoo.com>
2017-07-26 14:17:34 +0100

    feature(create-routes-and-api-endpoints): add dotenv package to hold environment variables

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const http = require('http');
 const app = express();
 
 const router = express.Router();
+require('dotenv').config();
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -25,7 +26,7 @@ app.use('/api/v1', router);
 
 // Send a default catch-all route
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to my greatness.',
+  message: 'Error. Please check URL and try again.',
 }));
 
 const port = parseInt(process.env.PORT, 10) || 8000;
