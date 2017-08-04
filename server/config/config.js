e3aaabd81833f785bbea211c8b70df08a57b89c4
Baasbank <baasb4real@yahoo.com>
2017-08-04 18:52:21 +0100

    chore(integrate-travis): set up travis.yml file add coveralls to dependencies set up test environment

--- server/config/config.js
+++ server/config/config.js
@@ -10,16 +10,8 @@ module.exports = {
     dialect: 'postgres'
   },
   test: {
-    username: 'postgres',
-    password: null,
-    database: 'test',
-    host: '127.0.0.1',
-    port: '5444',
-    dialect: 'postgres'
+    use_env_variable: 'TEST_DB'
   },
-  // test: {
-  //   use_env_variable: 'TEST_DB'
-  // },
   production: {
     use_env_variable: 'DATABASE_URL'
   }
