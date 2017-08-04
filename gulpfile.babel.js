8a74bbc3ca6ae2a8b7c315a48307b2a058d619c2
Baasbank <baasb4real@yahoo.com>
2017-08-04 18:34:09 +0100

    chore(integrate-gulp): refactor gulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp';
 import loadPlugins from 'gulp-load-plugins';
 import path from 'path';
-import coveralls from 'gulp-coveralls';
 import shell from 'gulp-shell';
 
 // Load the gulp plugins into the `plugins` variable
@@ -9,7 +8,7 @@ const plugins = loadPlugins();
 
 // Compile all Babel Javascript into ES5 and place in dist folder
 const paths = {
-  js: ['./**/*.js', '!dist/**', '!node_modules/**', '!coverage']
+  js: ['./**/*.js', '!dist/**', '!node_modules/**']
 };
 
 // Compile all Babel Javascript into ES5 and put it into the dist dir
@@ -18,18 +17,6 @@ gulp.task('babel', () =>
     .pipe(plugins.babel())
     .pipe(gulp.dest('dist'))
 );
-
-gulp.task('migrate', shell.task([
-  'cross-env NODE_ENV=test sequelize db:migrate',
-]));
-
-gulp.task('coverage', shell.task([
-  'cross-env NODE_ENV=test nyc mocha ./server/tests/**/*.js',
-]));
-
-gulp.task('coveralls', () => gulp.src('./coverage/lcov')
-  .pipe(coveralls()));
-
 
 // Restart server with on every changes made to file
 gulp.task('nodemon', ['babel'], () =>
@@ -41,6 +28,23 @@ gulp.task('nodemon', ['babel'], () =>
   })
 );
 
-gulp.task('test', ['migrate', 'coverage']);
 gulp.task('default', ['nodemon']);
 gulp.task('production', ['babel']);
+
+gulp.task('cleardb', shell.task([
+  'cross-env NODE_ENV=test sequelize db:migrate:undo:all',
+]));
+
+gulp.task('migrate', ['cleardb'], shell.task([
+  'cross-env NODE_ENV=test sequelize db:migrate',
+]));
+
+gulp.task('seed', ['migrate'], shell.task([
+  'cross-env NODE_ENV=test sequelize db:seed:all',
+]));
+
+gulp.task('coverage', ['seed'], shell.task([
+  'cross-env NODE_ENV=test nyc mocha ./server/tests/**/*.js --timeout 20000',
+]));
+
+gulp.task('test', ['coverage']);
